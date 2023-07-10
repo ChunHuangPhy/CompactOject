8114cf6725430f44d86645c350bc47144d3208fd
Nicole Osborn <nicoleosborn@dhcp-10-103-248-4.wireless.northwestern.private>
2023-07-10 15:20:39 -0500

    removed functionality

--- TOV_solver.py
+++ TOV_solver.py
@@ -29,20 +29,3 @@ def solveTOV(rhocent, eps, pres):
         dpdr, dmdr = TOV(sy.t+dr, stateTOV, inveos)
         dr = 0.46 * (1./stateTOV[1] * dmdr - 1./stateTOV[0]*dpdr)**(-1.)
     return stateTOV[1]*c**2./G/Msun, sy.t/1e5
-RFSU2R = []
-MFSU2R = []
-density = numpy.logspace(14.3, 15.6, 50)
-if   all(x<y for x, y in zip(eps_total_poly[j][:], eps_total_poly[j][1:])) and all(x<y for x, y in zip(pres_total_poly[j][:], pres_total_poly[j][1:])):
-    for i in range(len(density)):
-        try:
-            RFSU2R[j].append(solveTOV(density[i], eps_total_poly[j], pres_total_poly[j])[1])
-            MFSU2R[j].append(solveTOV(density[i], eps_total_poly[j], pres_total_poly[j])[0])
-        except OverflowError as e:
-            break
-        if i > 20 and solveTOV(density[i], eps_total_poly[j], pres_total_poly[j])[0] - solveTOV(density[i-1], eps_total_poly[j], pres_total_poly[j])[0]< 0:
-            break
-    if len(RFSU2R[j]) == False:
-        break
-    else:
-        del RFSU2R[j][-1]
-        del MFSU2R[j][-1]
