04aff295cb85510c720eddb9f7a9f38dd7654785
Nicole Osborn <nicoleosborn@dhcp-10-103-248-4.wireless.northwestern.private>
2023-07-10 14:46:03 -0500

    added main

---
+++ main.py
@@ -0,0 +1,16 @@
+# Python packages
+import numpy as np
+import math
+from scipy.interpolate import UnivariateSpline
+from scipy.constants import pi
+from scipy.integrate import odeint, ode
+from matplotlib import pyplot
+from scipy import optimize
+from itertools import repeat
+
+# Import files
+import TOV_solver
+
+# Global Variables
+pi = np.pi
+
