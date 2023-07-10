59cfdbd85cc15888931087415b7a314b18c5acef
9 <a9@9deMacBook-Air.local>
2023-07-10 18:03:38 -0500

    add a function named OutputMR, which will give you the Mass radius csv and the array as a output. Still need to figure out what is the input should be, maybe the path but how to send that

--- main.py
+++ main.py
@@ -14,17 +14,21 @@ import TOV_solver
 import EOS_import
 
 # Global Variables
-c = 3e10
-G = 6.67428e-8
-Msun = 1.989e33
+def OutputMR(path):
+    c = 3e10
+    G = 6.67428e-8
+    Msun = 1.989e33
 
-dyncm2_to_MeVfm3 = 1./(1.6022e33)
-gcm3_to_MeVfm3 = 1./(1.7827e12)
-oneoverfm_MeV = 197.33
-
-energy_density, pressure = EOS_import()
-
-def OutputMR(energy_density, pressure):
+    dyncm2_to_MeVfm3 = 1./(1.6022e33)
+    gcm3_to_MeVfm3 = 1./(1.7827e12)
+    oneoverfm_MeV = 197.33
+    
+    #############This is something we need to change, like the input for this EOS import should
+    ############# be one file contatining Whole EOS. that first column is density and second is pressure
+    energy_density, pressure = EOS_import(path)
+    ############# Lets the user only input the EOS file path, then this EOS_import should have file
+    ############# as input. and the outputMR should have a file as input too?
+    
     RFSU2R = []
     MFSU2R = []
     density = numpy.logspace(14.3, 15.6, 50)
@@ -33,8 +37,8 @@ def OutputMR(energy_density, pressure):
 #if   all(x<y for x, y in zip(eps_total_poly[:], eps_total_poly[[1:])) and all(x<y for x, y in zip(pres_total_poly[j][:], pres_total_poly[j][1:])):
     for i in range(len(density)):
         try:
-            RFSU2R.append(TOV_solver.solveTOV(density[i], energy_density[j], pressure[j])[1])
-            MFSU2R.append(TOV_solver.solveTOV(density[i], energy_density[j], pressure[j])[0])
+            RFSU2R.append(TOV_solver.solveTOV(density[i], energy_density, pressure)[1])
+            MFSU2R.append(TOV_solver.solveTOV(density[i], energy_density, pressure)[0])
     #This is sentense is for avoiding the outflow of the result, like when solveTOV blow up because of ill EOS, we need to stop
         except OverflowError as e:
             print("This EOS is ill-defined to reach a infinity result, that is not phyiscal, No Mass radius will be generated.")
