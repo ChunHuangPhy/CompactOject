f76f36690e440c39630d354dcc2915e911fc936a
Nicole Osborn <nicoleosborn@Nicoles-MacBook-Pro.local>
2023-07-11 15:11:54 -0500

    added speed of sound and fixed eos import and check functionalies

--- EoS_import.py
+++ EoS_import.py
@@ -1,13 +1,53 @@
+import csv
+import numpy as np
+
+def EOS_import(file_name = "", density = 0, pressure = 0):
+    
+    if not file_name:
+        density_checked, pressure_checked = EOS_check(density, pressure)
+        return density_checked, pressure_checked
+
+    input_file = file_name
+
+    density, pressure = file_read(input_file)
+
+    density_checked, pressure_checked = EOS_check(density, pressure)
+
+    return density, pressure
+
 def file_read(input_file):
     data_list = []
-    with open(input_file, newline='\n') as csvfile:
-        file_read = csv.reader(csvfile, delimiter=',', quotechar='|')
+    density_list = []
+    pressure_list = []
+    with open(input_file) as csvfile:
+        file_read = csv.reader(csvfile, delimiter=' ')
         data_list = [row for row in file_read]
-    pressure_list = [el for el in data_list[el][0]]
-    radius_list = [el for el in data_list[el][1]]
+    for row in data_list:
+        density_list.append(float(row[0]))
+        pressure_list.append(float(row[1]))
 
-    return pressure_list, radius_list
-def EOS_import(input_file):
-    pressure, radius = file_read(input_file)
+    # Make the lists numpy arrays 
+    density_array = np.array(density_list)
+    pressure_array = np.array(pressure_list)
 
-    return pressure,radius
+    return density_array, pressure_array
+
+def EOS_check(density, pressure):
+
+   dydx = np.gradient(density,pressure)
+
+    for value in dydx:
+        if value < 0:
+            print("This is not a valid equation of state")
+            exit()
+        else:
+            print("This is a valid equation of state")
+            return density, pressure
+        
+    
+    
+
+
+#EOS_import(file_name='/Users/nicoleosborn/EoS_inference/Test_EOS.csv')
+EOS_import(density=[1,-4,-5,-5],pressure=[1,4,5,1])
+
--- main.py
+++ main.py
@@ -31,7 +31,7 @@ def OutputMR(input_file):
     
     RFSU2R = []
     MFSU2R = []
-    density = numpy.logspace(14.3, 15.6, 50)
+    density = np.logspace(14.3, 15.6, 50)
 #This following step is to make a dicision whether the EOS ingredients is always increase. We can do that outsie of this main to the 
 #EOS import.
 #if   all(x<y for x, y in zip(eps_total_poly[:], eps_total_poly[[1:])) and all(x<y for x, y in zip(pres_total_poly[j][:], pres_total_poly[j][1:])):
@@ -42,7 +42,7 @@ def OutputMR(input_file):
     #This is sentense is for avoiding the outflow of the result, like when solveTOV blow up because of ill EOS, we need to stop
         except OverflowError as e:
             print("This EOS is ill-defined to reach a infinity result, that is not phyiscal, No Mass radius will be generated.")
-    MR = numpy.vstack((RFSU2R, MFSU2R)).T
+    MR = np.vstack((RFSU2R, MFSU2R)).T
     print("Mass Radius file will be generated and stored as MassRadius.csv, and the 2-d array. The first column is Radoius, second one is mass")
-    numpy.savetxt("MassRadius.csv", MR)
+    np.savetxt("MassRadius.csv", MR)
     return MR
