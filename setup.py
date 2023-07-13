27e07d4ac63b5f49916c605a9957f4bea64a958e
9 <a9@9deMacBook-Air.local>
2023-07-13 16:09:58 -0500

    change to 1.1.1

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="CompactObject_TOV"
-    version="1.0.0"
+    name="CompactObject_TOV",
+    version="1.1.1",
     packages = find_packages()
 )
