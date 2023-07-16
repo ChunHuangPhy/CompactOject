Fix the speed of sound computation

TOVsolver/speed_of_sound.py
@@ -1,4 +1,5 @@
 import numpy as np
+from TOVsolver.constant import c,G
 
 def speed_of_sound_calc(density, pressure):
 
@@ -12,5 +13,17 @@ def speed_of_sound_calc(density, pressure):
         speed_of_sound (array): numpy 1Darray.
     """
 
-    speed_of_sound = np.gradient(density,pressure)
-    return speed_of_sound
+    speed_of_sound = []
+    density = density*G/c**2
+    pressure = pressure*G/c**4
+    
+    for i in range(0,len(density)-1):
+        speed_of_sound.append((density[i+1]-density[i])/(pressure[i+1]-pressure[i]))
+    p2 = []
+    C_s= []
+    #eps2 = []
+    for i in range(0,len(speed_of_sound)):
+        if pressure[i]> 2e-14:
+            p2.append(pressure[i])
+            C_s.append(speed_of_sound[i])
+    return C_s