119bfe8e64d583c6f770b93eff06e7ed57e4ec94
9 <a9@uvavpn-byodm-145-18-161-241.vpn.uva.nl>
2023-07-17 12:21:19 -0500

    add a function computing single MR point, add on

--- TOVsolver/main.py
+++ TOVsolver/main.py
@@ -151,14 +151,13 @@ def OutputMRpoint(central_density,energy_density,pressure):
 #This following step is to make a dicision whether the EOS ingredients is always increase. We can do that outsie of this main to the 
 #EOS import.
 #if   all(x<y for x, y in zip(eps_total_poly[:], eps_total_poly[[1:])) and all(x<y for x, y in zip(pres_total_poly[j][:], pres_total_poly[j][1:])):
-    for i in range(len(density)):
-        try:
-            Radius.append(TOV_solver.solveTOV(density[i], energy_density, pressure)[1])
-            Mass.append(TOV_solver.solveTOV(density[i], energy_density, pressure)[0])
+    try:
+        Radius.append(TOV_solver.solveTOV(central_density, energy_density, pressure)[1])
+        Mass.append(TOV_solver.solveTOV(central_density, energy_density, pressure)[0])
     #This is sentense is for avoiding the outflow of the result, like when solveTOV blow up because of ill EOS, we need to stop
-        except OverflowError as e:
-            print("This EOS is ill-defined to reach an infinity result, that is not phyiscal, No Mass radius will be generated.")
+    except OverflowError as e:
+        Mass = []
+        Radius = []
     MR = np.vstack((Radius, Mass)).T
-    print("Mass Radius file will be generated and stored as  2-d array. The first column is Radius, second one is mass")
     
     return MR
