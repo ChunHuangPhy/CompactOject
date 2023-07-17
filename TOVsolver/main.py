4782a2ebea256496af3b038b59539d05fbefdc4a
9 <a9@9deMacBook-Air.local>
2023-07-17 18:41:20 -0500

    check wether there is problem, debug

--- TOVsolver/main.py
+++ TOVsolver/main.py
@@ -147,7 +147,7 @@ def OutputMRpoint(central_density,energy_density,pressure):
     
     Radius = []
     Mass = []
-    print("generated empty mr")
+    
 #This following step is to make a dicision whether the EOS ingredients is always increase. We can do that outsie of this main to the 
 #EOS import.
 #if   all(x<y for x, y in zip(eps_total_poly[:], eps_total_poly[[1:])) and all(x<y for x, y in zip(pres_total_poly[j][:], pres_total_poly[j][1:])):
@@ -158,5 +158,6 @@ def OutputMRpoint(central_density,energy_density,pressure):
     except OverflowError as e:
         print("This EOS is ill-defined to reach an infinity result, that is not phyiscal, No Mass radius will be generated.")
     MR = np.vstack((Radius, Mass)).T
-    print("should have some in mr")
+    if len(MR[0]) != 0:
+        print("should have some in mr")
     return MR
