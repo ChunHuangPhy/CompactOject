d3c6b5a2ecefe110e044964785f60c09812addf1
9 <a9@9deMacBook-Air.local>
2023-07-17 15:09:16 -0500

    delete resume

--- InferenceWorkflow/BayesianSampler.py
+++ InferenceWorkflow/BayesianSampler.py
@@ -3,7 +3,7 @@ import ultranest.stepsampler
 
 def UltranestSampler(parameters,likelihood,prior,nsteps,live_points,max_ncalls):
     
-    sampler = ultranest.ReactiveNestedSampler(parameters, likelihood, prior,log_dir='output',resume=True)
+    sampler = ultranest.ReactiveNestedSampler(parameters, likelihood, prior,log_dir='output')
     sampler.stepsampler = ultranest.stepsampler.SliceSampler(
         nsteps=nsteps,
         generate_direction=ultranest.stepsampler.generate_mixture_random_direction,
