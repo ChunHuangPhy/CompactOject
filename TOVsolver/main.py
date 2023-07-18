50f2049f239a5a5210e0d45767a10ebe91083add
9 <a9@9deMacBook-Air.local>
2023-07-18 13:14:21 -0500

    add single point Tidal solver

--- TOVsolver/main.py
+++ TOVsolver/main.py
@@ -158,6 +158,43 @@ def OutputMRpoint(central_density,energy_density,pressure):
     except OverflowError as e:
         print("This EOS is ill-defined to reach an infinity result, that is not phyiscal, No Mass radius will be generated.")
     MR = np.vstack((Radius, Mass)).T
-    if len(MR[0]) != 0:
-        print("should have some in mr")
+    
     return MR
+
+def OutputMRTpoint(central_density,energy_density,pressure):
+
+    """Outputs the mass, radius, and tidal deformability
+    Args:
+        file_name (string, optional): string. CSV file to be opened.
+        density (array, optional): numpy 1Darray. Passed into a check function and returned if valid.
+        pressure (array, optional): numpy 1Darray. Passed into a check function and returned if valid.
+
+    Returns:
+        MR (tuple): tuple with mass, radius.
+    """
+
+    c = 3e10
+    G = 6.67428e-8
+    Msun = 1.989e33
+
+    dyncm2_to_MeVfm3 = 1./(1.6022e33)
+    gcm3_to_MeVfm3 = 1./(1.7827e12)
+    oneoverfm_MeV = 197.33
+    
+    Radius = []
+    Mass = []
+    tidal = [] 
+#This following step is to make a dicision whether the EOS ingredients is always increase. We can do that outsie of this main to the 
+#EOS import.
+#if   all(x<y for x, y in zip(eps_total_poly[:], eps_total_poly[[1:])) and all(x<y for x, y in zip(pres_total_poly[j][:], pres_total_poly[j][1:])):
+    try:
+        Radius.append(TOV_solver.solveTOV_tidal(central_density, energy_density, pressure)[1])
+        Mass.append(TOV_solver.solveTOV_tidal(central_density, energy_density, pressure)[0])
+        tidal.append(TOV_solver.solveTOV_tidal(central_density, energy_density, pressure)[2])
+    #This is sentense is for avoiding the outflow of the result, like when solveTOV blow up because of ill EOS, we need to stop
+    except OverflowError as e:
+        print("This EOS is ill-defined to reach an infinity result, that is not phyiscal, No Mass radius will be generated.")
+    MRT = np.vstack((Radius, Mass,tidal)).T
+        
+    return MRT
+
