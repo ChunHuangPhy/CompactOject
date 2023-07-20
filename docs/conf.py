12e122d010a444d58e4c14c58dcd809b5beef95d
ChunHuangPhy <106232456+ChunHuangPhy@users.noreply.github.com>
2023-07-19 20:46:18 -0500

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -33,6 +33,7 @@ release = '1.6.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['sphinx.ext.autodoc',
+                'sphinx.ext.napoleon',
                 'sphinx.ext.intersphinx',
                 'sphinx.ext.viewcode',
                 'sphinx.ext.coverage',
