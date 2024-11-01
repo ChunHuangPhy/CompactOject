36e853e1710d983d724ce6181ab01719b1bd633e
zhoutz <519705198@qq.com>
2024-11-01 18:15:36 +0800

    remove unused files

---
+++ TOVsolver/unit.py
@@ -0,0 +1,26 @@
+fm = 1
+hbarc = 0.197327053
+c = 1  # speed of light
+hbar = 1  # reduced Planck constant
+
+GeV = 1 / hbarc  # Giga-electronvolt
+MeV = 1e-3 * GeV  # Mega-electronvolt
+
+g = 5.625e26 * MeV  # gram
+kg = 1e3 * g  # kilogram
+cm = 1e13 * fm  # centimeter
+m = 100 * cm  # meter
+km = 1e5 * cm  # kilometer
+s = 3e10 * cm  # second
+
+dyn = g * cm / s**2  # dyne
+dyn_cm_2 = dyn / cm**2  # dyne / cm^2
+g_cm_3 = g / cm**3  # gram / cm^3
+erg = dyn * cm  # ἐργον energy unit
+
+m_n = 939.565 * MeV  # mass of neutron
+n0 = 0.16 / fm**3  # saturation density
+
+e0 = m_n * n0  # saturation energy density
+G = 6.6743e-8 * dyn * cm**2 / g**2  # gravitational constant
+Msun = 1.989e33 * g  # mass of sun
