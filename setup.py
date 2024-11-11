8d39e5daf1c34e5213e3fac0f80593249ed1205d
ChunHuangPhy <chunhuang@mails.ccnu.edu.cn>
2024-11-11 13:57:15 -0600

    try to do automatically install

--- setup.py
+++ setup.py
@@ -1,7 +1,14 @@
 from setuptools import setup, find_packages
 
+# Function to read requirements from requirements.txt
+def parse_requirements(filename):
+    with open(filename, 'r') as f:
+        return f.read().splitlines()
+
 setup(
-    name="CompactObject_TOV",
-    version="1.9.6",
-    packages = find_packages()
+    name="CompactObject",
+    version="1.9.8",
+    packages=find_packages(),
+    install_requires=parse_requirements('requirements.txt'),  # Automatically adds requirements
+    # other setup parameters
 )
