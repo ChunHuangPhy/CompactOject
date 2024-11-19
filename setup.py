f22882d44ee57b9c1cfbef6ba6d5fc7b33ab3640
ChunHuangPhy <chunhuang@mails.ccnu.edu.cn>
2024-11-19 09:04:28 -0600

    add all the init.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def get_requires():
 
 setup(
     name="CompactObject-TOV",
-    version="1.9.9",
+    version="2.0.0",
     packages=find_packages(),
     # install_requires=get_requires(),  # Automatically adds requirements
     # other setup parameters
